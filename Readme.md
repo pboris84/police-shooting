7649ddb23ab044233d03128786b164f84b7eec87
Boris Pavlov <bp5007@yahoo.com>
2015-07-06 15:44:17 -0700

    -A

--- Readme.md
+++ Readme.md
@@ -1,3 +1,10 @@
 # Police shootings
 
+
+Boris Pavlov #0841034 
+INFO 343 Challenge 4 Police Shooting Map 
+Instructor: Michael Freeman, sum2015
+Style Guide: http://www.w3schools.com/html/html5_syntax.asp 
+
+
 This repository is a challenge for the following [course](http://faculty.washington.edu/mikefree/info343/) at the University of Washington.  The instructions are described [here](http://faculty.washington.edu/mikefree/info343/#/challenges/police-shooting).
--- js/map.js
+++ js/map.js
@@ -1,32 +1,49 @@
-// Function to draw your map
-var drawMap() = function() {
+//Boris Pavlov #0841034 
+//INFO 343 Challenge 4 Police Shooting Map 
+//Instructor: Michael Freeman, sum2015
+//Style Guide: http://www.w3schools.com/html/html5_syntax.asp
 
-  // Create map and set viewd
- 
-
-  // Create an tile layer variable using the appropriate url
-
-  // Add the layer to your map
- 
-
-  // Execute your function to get data
- 
+var drawMap = function() {   
+	var	map = L.map('container'); 
+	map.setView([42,-100],3);
+	var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');   
+	layer.addTo(map);
+    getData(map);
 }
 
-// Function for getting data
-var getData = function() {
+var getData = function(map) {
 
-  // Execute an AJAX request to get the data in data/response.js
-
-
-  // When your request is successful, call your customBuild function
-
+var data; 
+	$.ajax({
+    	url:'../data/response.json', 
+    	type: "get",
+    	success:function(dat) {
+    	   data = dat
+  		   customBuild(map, data); 
+   		}, 
+        dataType:"json"
+	}) 
 }
 
-// Do something creative with the data here!  
-var customBuild = function() {
 
-  
+var customBuild = function(map, data) { 
+	data.map(function(d){ 
+
+ 	if (d['Hit or Killed?'] == "Killed") {
+ 		if (d['Armed or Unarmed?'] == "Unarmed") {
+
+         var circle = new L.circle([d.lat,d.lng], 200, {color:'red'}).addTo(map)  
+     	}else {
+     	 var circle = new L.circle([d.lat,d.lng], 200, {color:'black'}).addTo(map)  
+     	}
+    } else {
+    	var circle = new L.circle([d.lat,d.lng], 200, {color:'blue'}).addTo(map)   
+    }
+         var name = "<p> Victim: ".bold() +  d['Victim Name'] + "</p>";
+         var summaryText = "<p> Summary: ".bold() + d.Summary + "</p>"; 
+         var date = "<p> Date of Incident: ".bold() + d['Date Searched'] + "</p>"; 
+         var location = "<p> State: ".bold() + d.State + ", County: ".bold() + d.County + ", City: ".bold() + d.City + "</p>";
+         circle.bindPopup(name + summaryText + location + date);   
+     })
 }
-
 
